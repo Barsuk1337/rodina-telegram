c495c22fa1bd4a519e76b8c6cc2e1eb089cfb0c6
Tony <gas.ji@yandex.ru>
2024-04-08 18:36:48 +0300

    boost inc removed

--- Callback.cpp
+++ Callback.cpp
@@ -6,11 +6,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/ini_parser.hpp>
-#include <boost/filesystem/operations.hpp>
-#include <boost/filesystem/path.hpp>
-
 namespace Callback
 {
 	static AMX* amx_allowed = NULL;
